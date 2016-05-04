9dc6e1c8c8ae257bc228fbac0664a7bd19d3e1a0
Markus Danek <markus.danek@webducation.at>
2016-05-04 15:56:08 +0200

    created init method

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,2 +1,35 @@
-# Import
+# Imports
 import numpy as np
+
+class BackPropagationNetwork:
+    #
+    # Class members
+    #
+    layerCount = 0
+    shape = none
+    weights = []
+
+    #
+    # Class methods
+    #
+    def __init__(self, layerSize):
+
+        # Layer info
+        self.layerCount = len(layerSize)
+        self.shape = layerSize
+
+        # Input/Output data from last run
+        self._layerInput = []
+        self._layerOutput = []
+
+        # Create the weight arrays
+        for (11,12) in zip(layerSize[:-1], layerSize[1:]):
+            self.weights.append(np.random.normal(scale=0.01, size=(12,11+1)))
+
+#
+# If run as script, create a test object
+#
+if __name__ == "__main__":
+    bpn = BackPropagationNetwork((2,2,1))
+    print(bpn.shape)
+    print(bpn.weights)
