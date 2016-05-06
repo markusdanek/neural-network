532432bece0b29d3d90df5070be71be16a178458
Markus Danek <markus.danek@gmail.com>
2016-05-06 13:32:03 +0200

    running the network

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -24,7 +24,37 @@ class BackPropagationNetwork:
 
         # Create the weight arrays
         for (11,12) in zip(layerSize[:-1], layerSize[1:]):
-            self.weights.append(np.random.normal(scale=0.01, size=(12,11+1)))
+            self.weights.append(np.random.normal(scale = 0.01, size = (12,11+1)))
+
+    #
+    # Run method
+    #
+    def Run(self, input):
+        """ Run the network based on the input data """
+        lnCases = input.shape[0]
+
+        # Clear out the previous intermediate value lists
+        self._layerInput = []
+        self._layerOutput = []
+
+        # Run it:
+        for index in range(self.layerCount):
+            # Determine layer input
+            if index == 0:
+                layerInput = self.weights[0].dot(np.vstack([input.T, np.ones([1, lnCases])]))
+            else:
+                layerInput = self.weights[index].dot(np.vstack([self._layerOutput[-1], np.ones([1, lnCases])]))
+            self._layerInput.append(layerInput)
+            self._layerOutput.append(self.sgm(layerInput))
+        return self._layerOutput[-1].T
+
+    # Transfer function
+    def sgm(self, x, Derivate = False):
+        if not Derivate:
+            return 1 / (1 + np.exp(-x))
+        else:
+            out = self.sgm(x)
+            return out * (1 - out)
 
 #
 # If run as script, create a test object
@@ -33,3 +63,8 @@ if __name__ == "__main__":
     bpn = BackPropagationNetwork((2,2,1))
     print(bpn.shape)
     print(bpn.weights)
+
+    lvInput = np.array([0,0], [1,1], [-1, 0.5])
+    lvOutput = bpn.Run(lvInput)
+
+    print("Input: {0}\nOutput: {1}".format(lvInput, lvOutput))
